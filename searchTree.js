8626cfe302d10de0031cf99eec906877c0b0db17
liujiang <1505460574@qq.com>
2021-12-29 15:54:01 +0800

    节点删除

--- searchTree.js
+++ searchTree.js
@@ -103,16 +103,82 @@ class BinarySearchTree{
             }
             if (key < node.key) {
                 node = node.left
-            }
-            if (key > node.key) {
+            } else {
                 node = node.right
             }
         }
         return null
     }
+    remove(key) {
+        let node = this.root
+        let parent = null;
+        let isLeft = false;
+        while(node.key !== key) {
+            parent = node
+            if (key < node.key) {
+                node = node.left
+                isLeft = true
+            } else {
+                node = node.right
+                isLeft = false
+            }
+            if (!node) {
+                return false
+            }
+        }
+        // 删除叶子节点
+        if (!node.left && !node.right) {
+            if (node === this.root) {
+                this.root = null
+            } else {
+                isLeft ? (parent.left = null) : (parent.right = null)
+            }
+        }
+        // 只有右子树
+        else if(!node.left){
+            if (node === this.root) {
+                this.root = this.root.right
+            } else {
+                isLeft ? (parent.left = node.right) : (parent.right = node.right)
+            }
+        }// 只有左子树
+        else if(!node.right) {
+            if (node === this.root) {
+                this.root = this.root.left
+            } else {
+                isLeft ? (parent.left = node.left) : (parent.right = node.left)
+            }
+        }
+        // 左右都有
+        else {
+            const replaceNode = this.getReplace(node)
+            replaceNode.left = node.left
+            replaceNode.right = node.right
+            if(node !== this.root) {
+                isLeft ? (parent.left = replaceNode) : (parent.right = replaceNode)
+            }
+        }
+        return true
+    }
+    // 寻找替换删除点的后继
+    getReplace(removeNode) {
+        let parent = removeNode;
+        let node = removeNode.right;
+        // 如果右侧节点直接就是替换的节点
+        if (!node.left) {
+            parent.right = node.right
+            return node
+        }
+        while(node.left) {
+            parent = node
+            node = node.left
+        }
+        parent.left = node.right
+        return node
+    }
 }
 
-res = ''
+let res = ''
 const ins = new BinarySearchTree()
 ins.insert(11)
 ins.insert(7)
@@ -124,20 +190,29 @@ ins.insert(5)
 ins.insert(4)
 ins.insert(23)
 ins.insert(1)
-ins.insert(25)
+ins.insert(30)
+// ins.insert(25)
 ins.insert(15)
 ins.insert(16)
+ins.insert(26)
+ins.insert(31)
 // console.log(ins.data);
-// ins.preOrderTraversal((key) => {
-//     res = res + key + '-'
-// });
+ins.preOrderTraversal((key) => {
+    res = res + key + '-'
+});
 // ins.midOrderTraversal((key) => {
 //     res = res + key + '-'
 // });
-ins.aftOrderTraversal((key) => {
-    res = res + key + '-'
-});
-console.log(ins.min);
-console.log(ins.max);
-console.log(ins.find(13));
+// ins.aftOrderTraversal((key) => {
+//     res = res + key + '-'
+// });
+// console.log(ins.min);
+// console.log(ins.max);
+// console.log(ins.find(13));
 console.log(res);
+console.log(ins.remove(23));
+let res2 = ''
+ins.preOrderTraversal((key) => {
+    res2 = res2 + key + '-'
+});
+console.log(res2);
